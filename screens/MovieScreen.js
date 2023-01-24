50a8a0aa2977b0eea3ead22af7c9fe1bdf587c28
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2023-01-24 23:39:33 +0530

    completed movies route

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,13 +1,18 @@
 // @ts-nocheck
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet,Pressable, FlatList } from 'react-native'
+import React, {useState} from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import HorizontalDatepicker from '@awrminkhodaei/react-native-horizontal-datepicker';
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { AntDesign } from '@expo/vector-icons';
+import malls from '../data/malls';
 const MovieScreen = () => {
   const route = useRoute()
   const navigation=useNavigation();
+   const [selectedDate, setSelectedDate] = useState("");
+  const [mall, setMall] = useState([]);
+  const [seatsData,setSeatsData] = useState([]);
+  const mallsData = malls;
   return (
     <SafeAreaView>
       <View style={{marginTop:37, padding:10, flexDirection:"row", alignItems:"center", justifyContent:"space-between"}}>
@@ -25,7 +30,7 @@ const MovieScreen = () => {
       <AntDesign name="Safety" size={24} color="black" />
       <Text style={{marginHorizontal:5, alignContent:"center"}}>Your Safety is our Priority</Text>
       </View>
-         <HorizontalDatepicker
+           <HorizontalDatepicker
         mode="gregorian"
         startDate={new Date("2022-08-24")}
         endDate={new Date("2022-08-30")}
@@ -41,6 +46,55 @@ const MovieScreen = () => {
         unselectedItemBackgroundColor="#ececec"
         flatListContainerStyle={styles.flatListContainerStyle}
       />
+      {mallsData.map((item, index) => (
+        <Pressable
+          onPress={() => {
+            setMall(item.name);
+            setSeatsData(item.tableData);
+          }}
+          style={{ margin: 10 }}
+          key={index}
+        >
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>{item.name}</Text>
+          {mall.includes(item.name) ? (
+            <FlatList
+              numColumns={3}
+              data={item.showtimes}
+              renderItem={({ item }) => (
+                <Pressable
+                onPress={() => navigation.navigate("Theatre",{
+                  mall:mall,
+                  name:route.params.name,
+                  timeSelected:item,
+                  tableSeats:seatsData,
+                  date:selectedDate,
+                  image:route.params.image
+                })}
+                  style={{
+                    borderColor: "green",
+                    borderWidth: 0.5,
+                    width: 80,
+                    borderRadius: 3,
+                    margin: 10,
+                    padding: 5,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 15,
+                      color: "green",
+                      fontWeight: "500",
+                      textAlign: "center",
+                    }}
+                  >
+                    {item}
+                  </Text>
+                </Pressable>
+              )}
+            />
+          ) : null}
+        </Pressable>
+      ))}
     </SafeAreaView>
   )
 }
